f1b73781e170ca424ae3a3b045d4b391ff720c9b
Razvan Sauciuc <razvansau9@gmail.com>
2025-12-24 00:48:45 +0200

    chore: added shadcn/ui components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
 
   return (
     <main>
       <h1 className="text-green-500">OK</h1>
+
+      <Button>
+        Click me!
+      </Button>
     </main>
   );
 }

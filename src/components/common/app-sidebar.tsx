c6a3697d2db18c9978138bd87ec7a130b0498b68
Razvan S. <razvansau9@gmail.com>
2026-01-18 13:14:11 +0200

    feat: added translations and unverified badge

--- src/components/common/app-sidebar.tsx
+++ src/components/common/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { Plus } from "lucide-react";
+import { BadgeAlert, Plus } from "lucide-react";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import { Separator } from "../ui/separator";
 import {
@@ -14,10 +14,14 @@ import {
 import Contact from "./contact";
 import { useAuth } from "@/auth/AuthContext";
 import useSWR from "swr";
+import { Button } from "../ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { useTranslation } from "react-i18next";
 
 type AccountMe = {
   username: string;
   full_name?: string | null;
+  verified?: string | null;
 };
 
 // Source - https://stackoverflow.com/a
@@ -39,7 +43,7 @@ const stringToColor = (str: string) => {
 function initialsFromName(name: string) {
   const parts = name.trim().split(/\s+/).filter(Boolean);
   const first = parts[0]?.[0] ?? "";
-  const last = parts.length > 1 ? parts[parts.length - 1]?.[0] ?? "" : "";
+  const last = parts.length > 1 ? (parts[parts.length - 1]?.[0] ?? "") : "";
   const initials = (first + last).toUpperCase();
   return initials || "?";
 }
@@ -57,6 +61,8 @@ export default function AppSidebar() {
   const storedSidebarPos = localStorage.getItem("amber.sidebarPos");
   const sidebarSide: "left" | "right" =
     storedSidebarPos === "right" ? "right" : "left";
+
+  const { t } = useTranslation();
 
   if (!isAuthenticated) return <>Unauthorized.</>;
 
@@ -80,47 +86,66 @@ export default function AppSidebar() {
         </SidebarContent>
         <SidebarFooter className="mb-2 mr-2 mt-0 ml-0 w-full">
           <Separator />
-          <section className="flex flex-row items-center gap-2 cursor-pointer rounded-md hover:bg-background p-2 transition ease-in-out duration-300">
-            <Avatar>
-              <AvatarFallback
-                style={
-                  account?.full_name
-                    ? {
-                        backgroundColor: stringToColor(
-                          account.full_name ?? "unknown"
-                        ),
-                      }
-                    : undefined
-                }
-              >
-                {account?.full_name
-                  ? initialsFromName(String(account.full_name))
-                  : account?.username
-                  ? initialsFromName(account.username)
-                  : isLoading
-                  ? "…"
-                  : "?"}
-              </AvatarFallback>
-            </Avatar>
-            <div className="flex flex-col items-start gap-1">
-              <h3 className="text-md leading-tight">
-                {error
-                  ? "Failed to load"
-                  : isLoading
-                  ? "Loading…"
-                  : (account?.full_name ?? "") || ""}
-              </h3>
-              <p className="text-muted-foreground text-xs">
-                {error
-                  ? ""
-                  : isLoading
-                  ? ""
-                  : account?.username
-                  ? `@${account.username}`
-                  : ""}
-              </p>
-            </div>
-          </section>
+          <div className="inline-flex items-center justify-between">
+            <section className="flex flex-row items-center gap-2 cursor-pointer rounded-md hover:bg-background p-2 transition ease-in-out duration-300">
+              <Avatar>
+                <AvatarFallback
+                  style={
+                    account?.full_name
+                      ? {
+                          backgroundColor: stringToColor(
+                            account.full_name ?? "unknown",
+                          ),
+                        }
+                      : undefined
+                  }
+                >
+                  {account?.full_name
+                    ? initialsFromName(String(account.full_name))
+                    : account?.username
+                      ? initialsFromName(account.username)
+                      : isLoading
+                        ? "…"
+                        : "?"}
+                </AvatarFallback>
+              </Avatar>
+              <div className="flex flex-col items-start gap-1">
+                <h3 className="text-md leading-tight">
+                  {error
+                    ? "Failed to load"
+                    : isLoading
+                      ? "Loading…"
+                      : (account?.full_name ?? "") || ""}
+                </h3>
+                <p className="text-muted-foreground text-xs">
+                  {error
+                    ? ""
+                    : isLoading
+                      ? ""
+                      : account?.username
+                        ? `@${account.username}`
+                        : ""}
+                </p>
+              </div>
+            </section>
+            {!account?.verified && (
+              <>
+                <Tooltip>
+                  <TooltipTrigger>
+                    <Button
+                      variant="ghost"
+                      className="cursor-pointer text-yellow-500"
+                    >
+                      <BadgeAlert />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>{t("register.verify.not_verified")}</p>
+                  </TooltipContent>
+                </Tooltip>
+              </>
+            )}
+          </div>
         </SidebarFooter>
       </Sidebar>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./i18n";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // Use contextBridge

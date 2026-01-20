9e4192f0b4e5f38d041ee54cf2e70b8b6b8f88e9
Razvan S. <razvansau9@gmail.com>
2026-01-20 14:55:23 +0200

    feat(change-email): updated UI to work with verification steps

--- src/views/settings/tabs/dialogs/ChangeEmail.tsx
+++ src/views/settings/tabs/dialogs/ChangeEmail.tsx
@@ -9,6 +9,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { API_BASE_URL } from "@/config";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
@@ -21,38 +22,133 @@ export default function ChangeEmail() {
   const { mutate } = useSWRConfig();
 
   const [open, setOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState<string | null>(null);
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState<string | null>(null);
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [confirmCode, setConfirmCode] = useState("");
+  const [verifyCode, setVerifyCode] = useState("");
+  const [stage, setStage] = useState(0);
+  const [isConfirmed, setIsConfirmed] = useState(false);
 
   const onSubmit = async () => {
     setError(null);
     setIsSubmitting(true);
 
     try {
-      const res = await fetch(API_BASE_URL + "/account/modify/email", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-          new_email: email,
-          password: password,
-        }),
-      });
+      switch (stage) {
+        case 0: {
+          const res = await fetch(
+            API_BASE_URL + "/account/modify/email/request",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+              },
+              body: JSON.stringify({
+                new_email: email,
+                password: password,
+              }),
+            },
+          );
 
-      const data = await res.json().catch(() => null);
-      setError(data?.detail);
+          const data = await res.json().catch(() => null);
 
-      if (res.ok) {
-        toast.success(t("settings.account.email.updated"));
-        await mutate("/account/me");
-        setEmail("");
-        setPassword("");
-        setOpen(false);
+          if (!res.ok) {
+            if (res.status === 429) {
+              throw new Error("common.errors.too_many_requests");
+            }
+
+            const detail = data?.detail;
+            throw new Error(detail);
+          }
+
+          toast.success(t("settings.account.email.verify_sent"));
+          setStage(1);
+          break;
+        }
+        case 1: {
+          if (isConfirmed) {
+            setStage(2);
+            break;
+          }
+
+          const res = await fetch(
+            API_BASE_URL + "/account/modify/email/confirm",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+              },
+              body: JSON.stringify({
+                code: confirmCode,
+              }),
+            },
+          );
+
+          const data = await res.json().catch(() => null);
+
+          if (!res.ok) {
+            if (res.status === 429) {
+              throw new Error("common.errors.too_many_requests");
+            }
+
+            const detail = data?.detail;
+            throw new Error(detail);
+          }
+
+          setIsConfirmed(true);
+          setStage(2);
+          break;
+        }
+        case 2: {
+          const res = await fetch(
+            API_BASE_URL + "/account/modify/email/verify",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${accessToken}`,
+              },
+              body: JSON.stringify({
+                code: verifyCode,
+              }),
+            },
+          );
+
+          const data = await res.json().catch(() => null);
+
+          if (!res.ok) {
+            if (res.status === 429) {
+              throw new Error("common.errors.too_many_requests");
+            }
+
+            const detail = data?.detail;
+            throw new Error(detail);
+          }
+
+          toast.success(t("settings.account.email.updated"));
+
+          setOpen(false);
+          setStage(0);
+          setIsConfirmed(false);
+
+          setEmail("");
+          setPassword("");
+          setConfirmCode("");
+          setVerifyCode("");
+
+          await mutate("/account/me");
+          break;
+        }
+        default: {
+          setStage(0);
+          setIsConfirmed(false);
+          break;
+        }
       }
     } catch (e) {
       setError(e instanceof Error ? e.message : "An error occured");
@@ -63,6 +159,13 @@ export default function ChangeEmail() {
 
   if (!isAuthenticated) return <>Unauthorized.</>;
 
+  const descriptionKey =
+    stage == 0
+      ? "settings.account.email.request_description"
+      : stage == 1
+        ? "settings.account.email.confirm_description"
+        : "settings.account.email.verify_description";
+
   return (
     <>
       <Dialog open={open} onOpenChange={setOpen}>
@@ -72,60 +175,123 @@ export default function ChangeEmail() {
               {t("settings.account.email.title")}
             </Button>
           </DialogTrigger>
-          <DialogContent className="sm:max-w-125 min-h-50 max-h-75 flex flex-col items-start justify-start">
+          <DialogContent className="sm:max-w-125 min-h-25 max-h-100 flex flex-col items-start justify-start">
             <DialogHeader>
               <DialogTitle>{t("settings.account.email.title")}</DialogTitle>
-              <DialogDescription>
-                {t("settings.account.email.description")}
-              </DialogDescription>
+              <DialogDescription>{t(descriptionKey)}</DialogDescription>
             </DialogHeader>
-            {/* content */}
-            <Input
-              placeholder={t("register.emailPlaceholder")}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              disabled={isSubmitting}
-              onKeyDown={(event) => {
-                if (event.key === "Enter") {
-                  onSubmit();
-                }
-              }}
-            />
+            {stage == 0 && (
+              <>
+                <Label>{t("register.emailPlaceholder")}</Label>
+                <Input
+                  placeholder={t("register.emailPlaceholder")}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  disabled={isSubmitting}
+                  onKeyDown={(event) => {
+                    if (event.key === "Enter") {
+                      onSubmit();
+                    }
+                  }}
+                />
 
-            <Input
-              placeholder={t("login.passwordPlaceholder")}
-              type={"password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              disabled={isSubmitting}
-              onKeyDown={(event) => {
-                if (event.key === "Enter") {
-                  onSubmit();
-                }
-              }}
-            />
+                <Input
+                  placeholder={t("login.passwordPlaceholder")}
+                  type={"password"}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  disabled={isSubmitting}
+                  onKeyDown={(event) => {
+                    if (event.key === "Enter") {
+                      onSubmit();
+                    }
+                  }}
+                />
+              </>
+            )}
+
+            {stage == 1 && (
+              <>
+                <Label>{t("settings.account.email.confirm_code")}</Label>
+                <Input
+                  placeholder={t("settings.account.email.confirm_code")}
+                  value={confirmCode}
+                  onChange={(e) => setConfirmCode(e.target.value)}
+                  disabled={isSubmitting}
+                  onKeyDown={(event) => {
+                    if (event.key === "Enter") {
+                      onSubmit();
+                    }
+                  }}
+                />
+              </>
+            )}
+
+            {stage == 2 && (
+              <>
+                <Label>{t("settings.account.email.verify_code")}</Label>
+                <Input
+                  placeholder={t("settings.account.email.verify_code")}
+                  value={verifyCode}
+                  onChange={(e) => setVerifyCode(e.target.value)}
+                  disabled={isSubmitting}
+                  onKeyDown={(event) => {
+                    if (event.key === "Enter") {
+                      onSubmit();
+                    }
+                  }}
+                />
+              </>
+            )}
 
             {error && <p className="text-red-500">{t(error)}</p>}
 
-            <div className="w-full inline-flex justify-end gap-1">
-              <Button
-                variant="outline"
-                className="cursor-pointer"
-                type="button"
-                onClick={() => setOpen(false)}
-              >
-                {t("common.cancel")}
-              </Button>
-              <Button
-                variant="default"
-                className="cursor-pointer"
-                disabled={isSubmitting}
-                type="button"
-                onClick={onSubmit}
-              >
-                {t("common.submit")}
-              </Button>
-            </div>
+            <section className="w-full inline-flex items-center justify-between gap-1">
+              <div className="inline-flex items-center gap-1 w-full text-2xl cursor-default text-muted-foreground select-none">
+                <div className={stage >= 0 ? "text-foreground" : undefined}>
+                  •
+                </div>
+                <div className={stage >= 1 ? "text-foreground" : undefined}>
+                  •
+                </div>
+                <div className={stage >= 2 ? "text-foreground" : undefined}>
+                  •
+                </div>
+              </div>
+              <div className="w-full inline-flex justify-end gap-1">
+                {stage <= 1 && (
+                  <Button
+                    variant="outline"
+                    className="cursor-pointer"
+                    type="button"
+                    onClick={() => setOpen(false)}
+                  >
+                    {t("common.cancel")}
+                  </Button>
+                )}
+                {stage > 1 && (
+                  <Button
+                    variant="outline"
+                    className="cursor-pointer"
+                    type="button"
+                    onClick={() => setStage(Math.max(1, stage - 1))}
+                  >
+                    {t("login.recovery.action.back")}
+                  </Button>
+                )}
+                <Button
+                  variant="default"
+                  className="cursor-pointer"
+                  disabled={isSubmitting}
+                  type="button"
+                  onClick={onSubmit}
+                >
+                  {stage == 2
+                    ? t("login.recovery.action.finish")
+                    : t("login.recovery.action.next")}
+                </Button>
+              </div>
+            </section>
           </DialogContent>
         </form>
       </Dialog>

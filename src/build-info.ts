2ff0ab7bff00c9a1efb76ece4f3b6b12cb3e0052
Razvan Sauciuc <razvansau9@gmail.com>
2026-03-19 09:18:41 +0200

    fix: wrong translation on friendship request

--- src/build-info.ts
+++ src/build-info.ts
@@ -1,4 +1,4 @@
-export const BUILD_ID = "20260319-0906";
-export const BUILD_VERSION = "26.3.19-h0906";
-export const BUILD_LABEL = "Built on March 19th, 2026 at 09:06 AM";
-export const BUILD_ISO = "2026-03-19T07:06:07.924Z";
+export const BUILD_ID = "20260319-0909";
+export const BUILD_VERSION = "26.3.19-h0909";
+export const BUILD_LABEL = "Built on March 19th, 2026 at 09:09 AM";
+export const BUILD_ISO = "2026-03-19T07:09:12.301Z";
